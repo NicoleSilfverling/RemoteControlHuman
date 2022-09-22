446af263c49b4f53775381a25755dfeba9825bba
Ograeset <tedros963@gmail.com>
2022-09-22 14:47:37 +0200

    Revert "expo-av"
    This reverts commit dfb8d85ec2dff130c4cc925308d6a614550eb271.

--- App.js
+++ App.js
@@ -82,7 +82,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
+    backgroundColor: "pink",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   layout: {
