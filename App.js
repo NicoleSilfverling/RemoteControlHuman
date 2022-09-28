9f385cb06cb3790987e7489688eb3ba6a2ab6f74
Ograeset <tedros963@gmail.com>
2022-09-28 10:18:07 +0200

    actionbar transparent

--- App.js
+++ App.js
@@ -149,7 +149,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "pink",
+    backgroundColor: "black",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   layout: {
