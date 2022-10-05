6f457e0bac18bdac5d44527b7d9f78b10bb70f1c
Nicole <nicole.silfverling@gmail.com>
2022-10-05 10:44:43 +0200

    hiddenbutton @media

--- components/HiddenButton.js
+++ components/HiddenButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
 
 const HiddenButton = () => {
   return <View style={styles.container}></View>;
@@ -7,15 +8,36 @@ const HiddenButton = () => {
 
 export default HiddenButton;
 
-const styles = StyleSheet.create({
+const styles = EStyleSheet.create({
   container: {
-    height: "12%",
+    height: "15%",
     aspectRatio: 1,
     borderRadius: 15,
-    borderWidth: 5,
+    borderWidth: 6,
     borderColor: "transparent",
     backgroundColor: "transparent",
     alignItems: "center",
     justifyContent: "center",
+  },
+  "@media (max-width: 1300)": {
+    container: {
+      height: "14%",
+      borderRadius: 10,
+      borderWidth: 4,
+    },
+  },
+  "@media (max-width: 1000)": {
+    container: {
+      height: "17%",
+      borderRadius: 10,
+      borderWidth: 3,
+    },
+  },
+  "@media (max-width: 900)": {
+    container: {
+      height: "17%",
+      borderRadius: 8,
+      borderWidth: 3,
+    },
   },
 });
